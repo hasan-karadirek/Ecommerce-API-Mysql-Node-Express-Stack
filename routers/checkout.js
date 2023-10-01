adceca062fbe0b6ac0f78259a6f77e663c2fb870
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2023-10-01 23:41:33 +0200

    checkout - molliepayment

---
+++ routers/checkout.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { checkout, mollieHook } = require('../controllers/checkout.js');
+const {
+  checkOrderExist,
+} = require('../middlewares/database/databaseErrorHandler.js');
+
+router.post('/', checkOrderExist, checkout);
+router.post('/mollie-hook', mollieHook);
+router.get('/thanks', (req, res) => {
+  return res.status(200).json({
+    message: 'thank you',
+  });
+});
+
+module.exports = router;
