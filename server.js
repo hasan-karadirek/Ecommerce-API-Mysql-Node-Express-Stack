c3dcd405883b7db0bb3b16c8723ade0630c1fa25
Hasan Karadirek <53180468+hasan-karadirek@users.noreply.github.com>
2023-10-11 02:25:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const swaggerConfig = require('./config/swaggerConfig');
 const path = require('path');
-const
 
 // Environment variables config
 dotenv.config({ path: './config/env/config.env' });
@@ -17,7 +16,7 @@ const sequelize = require('./helpers/database/connectDatabase');
 const customErrorHandler = require('./middlewares/errors/customErrorHandler.js');
 const app = express();
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', `${process.env.API_SERVER}`);
+  res.setHeader('Access-Control-Allow-Origin', `https://incredible-syrniki-af1d2c.netlify.app`);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
